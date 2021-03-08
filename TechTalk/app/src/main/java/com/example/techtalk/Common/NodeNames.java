ed6be4cdc9c1825568d4b7ef025621158124f0eb
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2021-03-09 01:49:00 +0530

    update profile image work done

---
+++ TechTalk/app/src/main/java/com/example/techtalk/Common/NodeNames.java
@@ -0,0 +1,10 @@
+package com.example.techtalk.Common;
+
+public class NodeNames {
+    public  static  final  String USERS = "Users";
+
+    public  static  final  String NAME = "name";
+    public  static  final  String EMAIL = "email";
+    public  static  final  String ONLINE = "online";
+    public  static  final  String PHOTO = "photo";
+}
